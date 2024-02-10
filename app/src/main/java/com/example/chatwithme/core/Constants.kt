789ab73b836e3c1ddd4edf72910d234de81a927c
Sidhik <abubakkarsidhik.mas@gmail.com>
2024-02-10 18:22:29 +0530

    Chat App First Push

---
+++ app/src/main/java/com/example/chatwithme/core/Constants.kt
@@ -0,0 +1,7 @@
+package com.example.chatwithme.core
+
+object Constants {
+    const val ERROR_MESSAGE = "Unexpected error."
+    const val NO_CHATROOM_IN_FIREBASE_DATABASE = "NO_CHATROOM_IN_FIREBASE_DATABASE"
+    const val ONESIGNAL_APP_ID = "f93dcc0f-ba86-4169-a6c2-4d5737f6cb5c"
+}
---
+++ app/src/main/java/com/example/chatwithme/presentation/chat/chatInput/ChatInput.kt
@@ -0,0 +1,216 @@
+package com.example.chatwithme.presentation.chat.chatInput
+
+import android.annotation.SuppressLint
+import android.widget.Toast
+import androidx.compose.foundation.focusable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.chatwithme.ui.theme.spacing
+
+@OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("UnrememberedMutableState")
+@Composable
+internal fun ChatInput(
+    modifier: Modifier = Modifier,
+    onMessageChange: (String) -> Unit,
+    onFocusEvent: (Boolean) -> Unit
+) {
+
+    val context = LocalContext.current
+
+    var input by remember { mutableStateOf(TextFieldValue("")) }
+    val textEmpty: Boolean by derivedStateOf { input.text.isEmpty() }
+
+//    val imePaddingValues = rememberInsetsPaddingValues(insets = LocalWindowInsets.current.ime)
+//    val imeBottomPadding = imePaddingValues.calculateBottomPadding().value.toInt()
+    val imePaddingValues = PaddingValues()
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(vertical = MaterialTheme.spacing.extraSmall),
+        verticalAlignment = Alignment.Bottom
+    ) {
+
+//        ChatTextField(
+//            modifier = modifier.weight(1f).focusable(true),
+//            input = input,
+//            empty = textEmpty,
+//            onValueChange = {
+//                input = it
+//            }, onFocusEvent = {
+//                onFocusEvent(it)
+//            }
+//        )
+//
+//        Spacer(modifier = Modifier.width(6.dp))
+        TextField(
+            modifier = Modifier
+                .clip(MaterialTheme.shapes.extraLarge)
+                .weight(1f)
+                .focusable(true),
+//                .padding(bottom = MaterialTheme.spacing.extraSmall),
+            value = input,
+            onValueChange = { input = it },
+            colors = TextFieldDefaults.textFieldColors(
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                disabledIndicatorColor = Color.Transparent
+            ),
+            placeholder = {
+                Text(text = "Message")
+            },
+            leadingIcon = {
+                IconButton(onClick = {
+
+                    Toast.makeText(
+                        context,
+                        "Emoji Clicked.\n(Not Available)",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }) {
+                    Icon(imageVector = Icons.Filled.Mood, contentDescription = "Mood")
+                }
+            },
+            trailingIcon = {
+                Row() {
+                    IconButton(onClick = {
+                        Toast.makeText(
+                            context,
+                            "Attach File Clicked.\n(Not Available)",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }) {
+                        Icon(imageVector = Icons.Filled.AttachFile, contentDescription = "File")
+                    }
+                    IconButton(onClick = {
+                        Toast.makeText(
+                            context,
+                            "Camera Clicked.\n(Not Available)",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }) {
+                        Icon(imageVector = Icons.Filled.CameraAlt, contentDescription = "Camera")
+                    }
+                }
+
+            }
+
+        )
+        FloatingActionButton(
+            shape = CircleShape,
+            onClick = {
+                if (!textEmpty) {
+                    onMessageChange(input.text)
+                    input = TextFieldValue("")
+                } else {
+                    Toast.makeText(
+                        context,
+                        "Sound Recorder Clicked.\n(Not Available)",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+        ) {
+            Icon(
+                imageVector = if (textEmpty) Icons.Filled.Mic else Icons.Filled.Send,
+                contentDescription = null
+            )
+        }
+    }
+}
+
+
+/*
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ComposeEmojiPickerDemo() {
+    val context = LocalContext.current
+    val sheetState = rememberModalBottomSheetState(
+        skipPartiallyExpanded = true,
+    )
+
+    var isModalBottomSheetVisible by remember {
+        mutableStateOf(false)
+    }
+    var selectedEmoji by remember {
+        mutableStateOf("😃")
+    }
+    var searchText by remember {
+        mutableStateOf("")
+    }
+
+    if (isModalBottomSheetVisible) {
+        ModalBottomSheet(
+            sheetState = sheetState,
+            shape = RectangleShape,
+            tonalElevation = 0.dp,
+            onDismissRequest = {
+                isModalBottomSheetVisible = false
+                searchText = ""
+            },
+            dragHandle = null,
+            windowInsets = WindowInsets(0),
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize(),
+            ) {
+                ComposeEmojiPickerBottomSheetUI(
+                    onEmojiClick = { emoji ->
+                        isModalBottomSheetVisible = false
+                        selectedEmoji = emoji.character
+                    },
+                    onEmojiLongClick = { emoji ->
+                        Toast.makeText(
+                            context,
+                            emoji.unicodeName.capitalizeWords(),
+                            Toast.LENGTH_SHORT,
+                        ).show()
+                    },
+                    searchText = searchText,
+                    updateSearchText = { updatedSearchText ->
+                        searchText = updatedSearchText
+                    },
+                )
+            }
+        }
+    }
+
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+    ) {
+        ComposeEmojiPickerEmojiUI(
+            emojiCharacter = selectedEmoji,
+            onClick = {
+                isModalBottomSheetVisible = true
+            },
+            fontSize = 56.sp,
+        )
+    }
+}*/
+
+@Composable
+fun EmojiKeyboardWrapper(
+    onEmojiSelected: (String) -> Unit
+) {
+
+    val context = LocalContext.current
+//  EmojiKeyboard(context, onEmojiSelected)
+}
+
